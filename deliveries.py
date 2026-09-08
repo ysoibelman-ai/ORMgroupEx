4fb2ca2c2bdd8955350b31336831f95c46702431
yaakov-ai <ysoibelman@gmail.com>
2026-09-08 14:18:10 +0300

    finished 8

--- deliveries.py
+++ deliveries.py
@@ -37,5 +37,37 @@ def delete_delivery(user:User,delivery_id:int) -> bool:
     except:
         raise Exception("couldnt delete delivery")
 
-    
 
+def get_info_to_create():
+    package=False
+    destination =False
+    weight=False
+    status = True
+    while status:
+        if package==False:
+            Package_name = input("enter Package name: ")
+            if val.check_name_package(Package_name) == False:
+                continue
+            else:
+                package = True
+        if destination ==False:
+            Destination= input("enter Destination of package: ")
+            if val.check_Destination(Destination) == False:
+                continue
+            else:
+                destination=True
+        if weight == False:
+            Weight= input("enter Weight of package: ")
+            if val.check_Weight(weight) == False:
+                continue
+            else:
+                weight=True
+        else:
+            status=False
+    return Package_name,Destination,Weight
+
+print(get_info_to_create())
+
+def create_delivery(user,package,destination,weight):
+    data = {"package_name":package,"destination":destination,"weight":weight,"owner":user}
+    Delivery.create(data)
