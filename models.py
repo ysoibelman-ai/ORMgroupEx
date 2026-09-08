1187a751cb0a33f50cdeeaad5eabad7eb756ead9
gad-biton <gadb633@gmail.com>
2026-09-08 10:00:24 +0300

    added class User

--- models.py
+++ models.py
@@ -5,3 +5,11 @@ from db import db
 class BaseModel(Model):
     class Meta:
         database = db
+
+class User(BaseModel):
+    id=PrimaryKeyField()
+    username=CharField(unique=True,null=False)
+    password_hash = CharField(unique=True)
+    created_at=DateTimeField(default=datetime.datetime.now)
+        
+
